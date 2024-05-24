35a4c592252819391477942a6d6f0b3813181ff0
dincerek <flukossmain@gmail.com>
2024-05-24 15:40:08 +0200

    updated links

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -34,21 +34,9 @@ const Sidebar = () => {
             className={` transition-transform duration-300 sm:translate-x-0 flex flex-col`}
           >
             <div className="flex flex-col gap-y-2 rounded-xl text-xl">
-              <Link href={socialMediaData.linkedin} target="_blank">
-                <FaLinkedin className="hover:text-[#FBBF64] transition-all shadow-black shadow-xl duration-200 w-[100%] h-[100%]" />
-              </Link>
-              <Link href={socialMediaData.twitter} target="_blank">
-                <FaTwitter className="hover:text-[#FBBF64] transition-all shadow-black shadow-xl duration-200 w-[100%] h-[100%]" />
-              </Link>
-              <Link href={socialMediaData.github} target="_blank">
-                <FaGithub className="hover:text-[#FBBF64] transition-all duration-200 w-[100%] h-[100%]" />
-              </Link>
-              <Link href={socialMediaData.email} target="_blank">
-                <FaEnvelope className="hover:text-[#FBBF64] transition-all duration-200 w-[100%] h-[100%]" />
-              </Link>
-              <div className="pt-2" onClick={() => setSidebar(!sidebar)}>
-                <TbSquareArrowLeftFilled className="text-4xl text-[#fbbf64]" />
-              </div>
+            <Link href={socialMediaData.linkedin} target="_blank">
+              <FaDiscord className="hover:text-[#ff0000] transition-all hover:scale-125 duration-500 w-[200%] h-[200%]" />
+            </Link>
             </div>
           </div>
         )}
